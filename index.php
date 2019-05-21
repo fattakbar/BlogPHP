65b6486b76c73af3b5e8ff93d3058cdcdc6ceccb
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-05-22 01:30:49 +0700

    Menambahkan File Header

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+    include "koneksi.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog Sederhana</title>
+    <link rel="icon" href="img/astrounout.png" />
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+     <link rel="stylesheet" href="bootstrap/css/style.css" />
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.css" />
+</head>
+<body>
+
+</body>
+</html>
