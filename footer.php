1e4202588d8576585e929b134200c1a1e54f1a81
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-05-22 09:41:16 +0700

    Menambahkan File footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="container wrap">
+    <div class="row">
+        <div class="col-md-6">
+            <h4><b>Kontak Saya</b></h4>
+            <p>Jl. Aru Indah</p>
+            <p>Telepon, WA: <tel>0823-8770-3772</tel>,<br> Email: <b>akbarfattahul22@gmail.com</b></p>
+            <br><br>
+        </div>
+    </div>
+</div>
