fd8d4c467dd11bf9d1bffee8f307cef2fb887473
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-05-22 10:02:23 +0700

    Menambahkan Header dan Footer Pada Tampilan Utama

--- index.php
+++ index.php
@@ -10,13 +10,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog Sederhana</title>
+    <title>Cerita Programmer Blog</title>
     <link rel="icon" href="img/astrounout.png" />
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
      <link rel="stylesheet" href="bootstrap/css/style.css" />
     <link rel="stylesheet" href="font-awesome/css/font-awesome.css" />
 </head>
 <body>
+    <header>
+        <?php include("header.php"); ?>
+    </header>
 
+    <footer>
+        <?php include("footer.php"); ?>
+    </footer>
 </body>
 </html>
