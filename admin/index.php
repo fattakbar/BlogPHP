775c70759838eef8086dc4958eca256836d2ffd2
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-05-22 20:35:15 +0700

    Menambahkan File Navbar Pada Halaman Admin Panel

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    session_start();
+
+    if(isset($_SESSION['username'])){
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Panel</title>
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../bootstrap/css/style-admin.css" rel="stylesheet">
+    <link href="../font-awesome/css/font-awesome.css" rel="stylesheet">
+</head>
+<body>
+    <nav></nav>
+</body>
+</html>
+
+<?php } ?>
