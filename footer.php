e5faa263bad5d62ce2bc89e6d660d154f2c077df
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-05-22 09:49:46 +0700

    Fix footer.php

--- footer.php
+++ footer.php
@@ -6,5 +6,19 @@
             <p>Telepon, WA: <tel>0823-8770-3772</tel>,<br> Email: <b>akbarfattahul22@gmail.com</b></p>
             <br><br>
         </div>
+        <div class="col-md-6">
+            <h4><b>Twitter @cerita_programmer</b></h4>
+            <div>
+                <p class="fa fa-lg fa-twitter" style="float: left; margin: 5px 10px"></p> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                <p class="fa fa-lg fa-twitter" style="float: left; margin: 5px 10px"></p> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                <p class="fa fa-lg fa-twitter" style="float: left; margin: 5px 10px"></p> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <hr>
+            <p>Hak Cipta &copy; <?php  echo Date("Y"); ?> Cerita Programmer</p>
+        </div>
     </div>
 </div>
