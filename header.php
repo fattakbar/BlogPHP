8852ea73727cbddcdc9068fb63f91ce8f8681efe
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-05-22 09:37:44 +0700

    Fix Header

---
+++ header.php
@@ -0,0 +1,13 @@
+<div class="container wrap">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="profil">
+                <img src="img/astrounout.png"/>
+                <div class="deskripsi">
+                    <h1><b>Cerita Programmer</b></h1>
+                    <p>Happy Coding!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
