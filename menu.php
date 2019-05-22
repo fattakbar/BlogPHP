ccbdeb455be7e980749fff09e64755c375b74480
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-05-22 10:25:30 +0700

    Fix Menu

---
+++ menu.php
@@ -0,0 +1,16 @@
+<div class="container wrap">
+    <div class="row">
+        <div class="col-md-12">
+            <ol class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="daftar_isi.php">Daftar isi</a></li>
+                <li><a href="about.php">About</a></li>
+            </ol>
+            <ol class="nav navbar-nav navbar-right">
+                <li><a href="" target="blank"><i class="fa fa-lg fa-facebook-square"></i></a></li>
+                <li><a href="" target="blank"><i class="fa fa-lg fa-twitter-square"></i></a></li>
+                <li><a href="" target="blank"><i class="fa fa-lg fa-github-square"></i></a></li>
+            </ol>
+        </div>
+    </div>
+</div>
